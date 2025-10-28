44c2a21d023aff2891158a5f3f6350e893b12c9e
Zach Dodson <zdodson21@gmail.com>
2025-10-28 14:53:45 -0400

    stuff working

--- jinja.py
+++ jinja.py
@@ -1,9 +1,15 @@
 from jinja2 import Environment, FileSystemLoader
-env = Environment(loader = FileSystemLoader('templates'))
+
+env = Environment(loader = FileSystemLoader('./'))
 template = env.get_template('template.html') # Change to template.html.j2 when done
+
 output = template.render(
   name = 'Test',
   test2 = 'Test2'
 )
 
-print(output)
+with open("weather.html", "w") as html:
+  html.write(output)
+
+
+# print(output)
